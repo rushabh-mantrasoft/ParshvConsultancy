9880a7996b1b0c06460716dc3223e071cf28eed5
rushabh-mantrasoft <rushabh@themantrasoft.com>
2025-09-27 10:33:27 +0530

    First Config changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'parshv_consultancy',
+  database: process.env.DB_NAME || 'consultancy',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
